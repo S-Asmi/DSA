3600c9bbd2712cb16ed0ac84332f27559341ad8d
Asmi Singh <155385717+S-Asmi@users.noreply.github.com>
2026-01-07 11:07:45 -0600

    Time: 51 ms (30.60%) | Memory: 14.1 MB (15.09%) - LeetSync

---
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def replaceElements(self, arr):
+        rightMax=-1
+        for i in range(len(arr)-1,-1,-1):
+            newMax=max(rightMax,arr[i])
+            arr[i]=rightMax
+            rightMax=newMax
+        return arr
