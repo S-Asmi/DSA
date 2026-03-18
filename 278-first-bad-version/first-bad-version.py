bcfd225c0a3a836fbec0d9849a690060f45a8fd8
Asmi Singh <155385717+S-Asmi@users.noreply.github.com>
2026-03-18 04:08:27 -0500

    Time: 11 ms (87.07%) | Memory: 12.3 MB (96.35%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def firstBadVersion(self, n):
+        l, r = 1, n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
