84fe411d1dd767ea68721c681686e0fbf17a5554
Asmi Singh <155385717+S-Asmi@users.noreply.github.com>
2026-08-01 10:51:01 -0500

    Time: 35 ms (43.67%) | Memory: 14 MB (87.72%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        s.sort()
+        g.sort()
+        count=0
+        i=0
+        j=0
+        while i<len(g) and j<len(s):
+            if g[i]<=s[j]:
+                count+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return count
+
+        
