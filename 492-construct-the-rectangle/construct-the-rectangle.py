e24c1c9a657f4bf5b6ade6434e9d6adda987e7a1
Asmi Singh <155385717+S-Asmi@users.noreply.github.com>
2026-09-13 11:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.06%) - LeetSync

---
+++ 492-construct-the-rectangle/construct-the-rectangle.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def constructRectangle(self, area):
+        mid = int(math.sqrt(area))
+        while mid > 0:
+            if area % mid == 0:
+                return [int(area / mid), int(mid)]
+            mid -= 1
